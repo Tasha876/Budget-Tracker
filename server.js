b8a62feb1fb885d74458e9af100ba4f8b5d74de8
Natasha Fray <natasha.fray9@gmail.com>
2021-02-27 00:45:11 +0000

    changed font-end slightly, added to my repo

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const uri = "mongodb+srv://nfray:JpNkUoBRVhuCEKue@cluster0.jhsyl.mongodb.net/budget-tracker?retryWrites=true&w=majority";
+
+mongoose.connect(
+    process.env.MONGODB_URI || uri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }, (e) => {
+      if (e) console.log(e)
+    }
+)
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
