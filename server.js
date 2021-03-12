6e0e7c38bbda11204ceff24a4a84744acf3952df
Natasha Fray <natasha.fray9@gmail.com>
2021-03-11 21:00:15 -0500

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = PROCESS.ENV.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
